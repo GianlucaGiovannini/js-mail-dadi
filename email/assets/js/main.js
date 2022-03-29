2e04f9dd065bc61167d8897d2eedd4141dadb1cb
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-29 18:46:05 +0200

    last commit

--- email/assets/js/main.js
+++ email/assets/js/main.js
@@ -8,7 +8,7 @@ const elementResult = document.querySelector(".result");
 elementValidator.addEventListener("click",
     function() {
 
-        let userMail = document.getElementById("mail").value;
+        let userMail = document.getElementById("email").value;
         let loginEnable = false;
 
         for (let i = 0; i < listEnableMail.length; i++) {
@@ -20,18 +20,19 @@ elementValidator.addEventListener("click",
         }
 
         let stamp;
+        let message;
 
         if (loginEnable == false) {
             stamp = "E-mail sbagliata, riprova ad inserirla"
+            message = `<h1 class="display-4 fw-bold color_red">E-mail sbagliata, riprova ad inserirla</h1>`
+
         } else {
             stamp = "Loggato con successo"
+            message = `<h1 class="display-4 fw-bold color_success">Loggato con successo</h1>`
         }
 
         alert(stamp);
-
-        elementResult.classList.remove("d-none")
-        elementResult.classList.add("bg_a_caso")
-        elementResult.innerHTML = stamp;
+        elementResult.innerHTML = message;
 
     }
 );
