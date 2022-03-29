552179a733e1e1097f4d1fea1e581837c36dfcc6
GianlucaGiovannini <gianluca-giova@hotmail.it>
2022-03-29 17:53:07 +0200

    Add style

--- email/assets/js/main.js
+++ email/assets/js/main.js
@@ -1,24 +1,37 @@
 /* Crea una lista con delle e-mail che possono accedere */
 const listEnableMail = ["gianluca@gmail.com", "giovannini@gmail.com", "user@gmail.com", "admin@gmail.com", "moderator@gmail.com", "default@gmail.com"];
 
-let userMail = prompt("Scrivi la tua e-mail per accedere");
-let loginEnable = false;
-/* se la sua e-mail è nella lista di chi può accedere allora stampa un messaggio appropriato sull'esito del controllo */
+const elementValidator = document.querySelector("button");
 
-for (let i = 0; i < listEnableMail.length; i++) {
-    let mailControl = listEnableMail[i];
+const elementResult = document.querySelector(".result");
 
-    if (userMail == mailControl) {
-        loginEnable = true;
+elementValidator.addEventListener("click",
+    function() {
+
+        let userMail = document.getElementById("mail").value;
+        let loginEnable = false;
+
+        for (let i = 0; i < listEnableMail.length; i++) {
+            let mailControl = listEnableMail[i];
+
+            if (userMail == mailControl) {
+                loginEnable = true;
+            }
+        }
+
+        let stamp;
+
+        if (loginEnable == false) {
+            stamp = "E-mail sbagliata, riprova ad inserirla"
+        } else {
+            stamp = "Loggato con successo"
+        }
+
+        alert(stamp);
+
+        elementResult.classList.remove("d-none")
+        elementResult.classList.add("bg_a_caso")
+        elementResult.innerHTML = stamp;
+
     }
-}
-
-let stamp;
-
-if (loginEnable == false) {
-    stamp = "E-mail sbagliata, riprova ad inserirla"
-} else {
-    stamp = "Loggato con successo"
-}
-
-alert(stamp);
+);
